6c8e8e0b4221fc91154b1a6a00422c3f9740098b
Tommy Nguyen <94974692+seobtr@users.noreply.github.com>
2024-03-30 19:36:46 -0500

    main

--- next.config.js
+++ next.config.js
@@ -1,45 +1,53 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    redirects : async () => {
-        return [
-            {
-                source: '/',
-                destination: '/home',
-                permanent: true,
-            },
-            {
-                source: '/old-blog/:slug',
-                // '/old-blog/overwatch' or '/old-blog/starcraft' will work
-                // '/old-blog/diablo/four' won't work
-                destination: '/news/:slug',
-                permanent: true,
-            },
-            {
-                source: '/old-blog/:slug*',
-                // Example: '/old-blog/diablo/four/why/people/like/it'
-                destination: '/news/:slug*',
-                permanent: true,
-            },
-            {
-                source: "/post/:slug(\\d{1,})",
-                // Example: '/post/123' but not '/post/abc'
-
-                // For nesting don’t forget to add "*” sign.
-                // Example: '/post/:slug(\\d{1,})*'
-                destination: "/news/:slug",
-                permanent: true,
-            },
-
-        ]
+  redirects: async () => {
+    return [
+    {
+        source: '/',
+        destination: '/home',
+        permanent: true,
     },
-    rewrites: async () => {
-        return [
-            {
-                source: '/home',
-                destination: '/rewrite',
-            },
-        ]
+    {
+        source: '/ingrown-toenail',
+        destination: '/home',
+        permanent: true,
     },
-}
+      {
+        source: '/old-blog/:slug',
+        // '/old-blog/overwatch' or '/old-blog/starcraft' will work
+        // '/old-blog/diablo/four' won't work
+        destination: '/news/:slug',
+        permanent: true,
+      },
+      {
+        source: '/old-blog/:slug*',
+        // Example: '/old-blog/diablo/four/why/people/like/it'
+        destination: '/news/:slug*',
+        permanent: true,
+      },
+      {
+        source: '/post/:slug(\\d{1,})',
+        // Example: '/post/123' but not '/post/abc'
 
-module.exports = nextConfig
+        // For nesting don’t forget to add "*” sign.
+        // Example: '/post/:slug(\\d{1,})*'
+        destination: '/news/:slug',
+        permanent: true,
+      },
+    ];
+  },
+  rewrites: async () => {
+    return [
+      {
+          source: '/ingrown-toenail',
+          destination: '/rewrite',
+      },
+      {
+        source: '/home',
+        destination: '/rewrite',
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
