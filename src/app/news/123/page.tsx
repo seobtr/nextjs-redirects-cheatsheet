4fff7810c4f8ad51d0a4adb297e0382d6738858e
Tommy Nguyen <94974692+seobtr@users.noreply.github.com>
2024-03-30 19:47:14 -0500

    main

--- src/app/news/123/page.tsx
+++ src/app/news/123/page.tsx
@@ -4,7 +4,7 @@ export default function Page123() {
     return (
         <main className={styles.main}>
             <div className={styles.description}>
-                <h1>Hello, I'm page "news/123"!</h1>
+                <h1>Hello, I'm page "news/123"</h1>
                 <p>You can see me only if redirect to "/news/:slug" is working</p>
             </div>
         </main>
